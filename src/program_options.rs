a327e4ae3fbc2eef66a64946d25dbb8b9c2b19a8
Samer Afach <info@afach.de>
2024-10-04 22:13:43 +0400

    Add more information for help

--- src/program_options.rs
+++ src/program_options.rs
@@ -1,6 +1,13 @@
 use clap::Parser;
 
 #[derive(Parser, Clone, Debug, Default)]
+#[command(
+    name = "rdgen",
+    version = env!("CARGO_PKG_VERSION"),
+    long_about = "A terminal program for generating reproducible random data for testing based on a provided seed.",
+    author = "TheQuantumPhysicist <https://github.com/TheQuantumPhysicist>",
+    after_help = r#"Pipe some seed into rdgen, specify the length of the output, to generate deterministic, random data, with any length you need. Example: echo -n "abc" | rdgen -l100 | xxd -p -c 0"#
+)]
 pub struct RDGenOptions {
     /// The length of the data to be output
     #[arg(long, short('l'), value_name("NUMBER"))]
